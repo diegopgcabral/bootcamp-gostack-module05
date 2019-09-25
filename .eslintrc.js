0561cdeea2a9445aaa554eb73bb9e55b9cb466b5
diegopgcabral <diegopgc@gmail.com>
2019-09-25 00:07:34 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
+    'react/no-unused-state': 'off',
   },
 };
